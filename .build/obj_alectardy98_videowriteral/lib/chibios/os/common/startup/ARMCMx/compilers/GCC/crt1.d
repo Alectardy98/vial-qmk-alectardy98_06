a77f144f3b944536c26868f35cf13987186a9c8a
Alectardy98 <61422584+Alectardy98@users.noreply.github.com>
2024-04-21 13:17:17 -0400

    Push Add Videowriteral

---
+++ .build/obj_alectardy98_videowriteral/lib/chibios/os/common/startup/ARMCMx/compilers/GCC/crt1.d
@@ -0,0 +1,62 @@
+.build/obj_alectardy98_videowriteral/lib/chibios/os/common/startup/ARMCMx/compilers/GCC/crt1.o: \
+ lib/chibios/os/common/startup/ARMCMx/compilers/GCC/crt1.c \
+ keyboards/alectardy98/videowriteral/config.h quantum/config_common.h \
+ platforms/pin_defs.h platforms/chibios/_pin_defs.h \
+ platforms/chibios/vendors/RP/_pin_defs.h quantum/audio/song_list.h \
+ quantum/audio/musical_notes.h \
+ .build/obj_alectardy98_videowriteral/src/info_config.h \
+ .build/obj_alectardy98_videowriteral/src/layouts.h \
+ keyboards/alectardy98/videowriteral/keymaps/vial/config.h \
+ platforms/chibios/boards/GENERIC_PROMICRO_RP2040/configs/config.h \
+ lib/chibios/os/common/startup/ARMCMx/devices/RP2040/cmparams.h \
+ platforms/chibios/boards/GENERIC_PROMICRO_RP2040/configs/board.h \
+ lib/chibios/os/hal/boards/RP_PICO_RP2040/board.h \
+ lib/chibios/os/common/ext/RP/RP2040/rp2040.h \
+ lib/chibios/os/common/ext/ARM/CMSIS/Core/Include/core_cm0plus.h \
+ lib/chibios/os/common/ext/ARM/CMSIS/Core/Include/cmsis_version.h \
+ lib/chibios/os/common/ext/ARM/CMSIS/Core/Include/cmsis_compiler.h \
+ lib/chibios/os/common/ext/ARM/CMSIS/Core/Include/cmsis_gcc.h \
+ lib/chibios/os/common/ext/ARM/CMSIS/Core/Include/mpu_armv7.h \
+ lib/chibios/os/common/ext/RP/RP2040/system_rp2040.h
+
+keyboards/alectardy98/videowriteral/config.h:
+
+quantum/config_common.h:
+
+platforms/pin_defs.h:
+
+platforms/chibios/_pin_defs.h:
+
+platforms/chibios/vendors/RP/_pin_defs.h:
+
+quantum/audio/song_list.h:
+
+quantum/audio/musical_notes.h:
+
+.build/obj_alectardy98_videowriteral/src/info_config.h:
+
+.build/obj_alectardy98_videowriteral/src/layouts.h:
+
+keyboards/alectardy98/videowriteral/keymaps/vial/config.h:
+
+platforms/chibios/boards/GENERIC_PROMICRO_RP2040/configs/config.h:
+
+lib/chibios/os/common/startup/ARMCMx/devices/RP2040/cmparams.h:
+
+platforms/chibios/boards/GENERIC_PROMICRO_RP2040/configs/board.h:
+
+lib/chibios/os/hal/boards/RP_PICO_RP2040/board.h:
+
+lib/chibios/os/common/ext/RP/RP2040/rp2040.h:
+
+lib/chibios/os/common/ext/ARM/CMSIS/Core/Include/core_cm0plus.h:
+
+lib/chibios/os/common/ext/ARM/CMSIS/Core/Include/cmsis_version.h:
+
+lib/chibios/os/common/ext/ARM/CMSIS/Core/Include/cmsis_compiler.h:
+
+lib/chibios/os/common/ext/ARM/CMSIS/Core/Include/cmsis_gcc.h:
+
+lib/chibios/os/common/ext/ARM/CMSIS/Core/Include/mpu_armv7.h:
+
+lib/chibios/os/common/ext/RP/RP2040/system_rp2040.h:
