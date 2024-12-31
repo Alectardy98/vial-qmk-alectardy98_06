91e2c28f2f99284f7df9a407701a2c8ce0b31228
Alectardy98 <61422584+Alectardy98@users.noreply.github.com>
2024-12-31 15:14:02 -0500

    push alienware

---
+++ .build/obj_alectardy98_alienware_rp2040/platforms/chibios/syscall-fallbacks.d
@@ -0,0 +1,32 @@
+.build/obj_alectardy98_alienware_rp2040/platforms/chibios/syscall-fallbacks.o: \
+ platforms/chibios/syscall-fallbacks.c \
+ keyboards/alectardy98/alienware/rp2040/config.h quantum/config_common.h \
+ platforms/pin_defs.h platforms/chibios/_pin_defs.h \
+ platforms/chibios/vendors/RP/_pin_defs.h quantum/audio/song_list.h \
+ quantum/audio/musical_notes.h \
+ .build/obj_alectardy98_alienware_rp2040/src/info_config.h \
+ .build/obj_alectardy98_alienware_rp2040/src/layouts.h \
+ keyboards/alectardy98/alienware/keymaps/vial/config.h \
+ platforms/chibios/boards/GENERIC_PROMICRO_RP2040/configs/config.h
+
+keyboards/alectardy98/alienware/rp2040/config.h:
+
+quantum/config_common.h:
+
+platforms/pin_defs.h:
+
+platforms/chibios/_pin_defs.h:
+
+platforms/chibios/vendors/RP/_pin_defs.h:
+
+quantum/audio/song_list.h:
+
+quantum/audio/musical_notes.h:
+
+.build/obj_alectardy98_alienware_rp2040/src/info_config.h:
+
+.build/obj_alectardy98_alienware_rp2040/src/layouts.h:
+
+keyboards/alectardy98/alienware/keymaps/vial/config.h:
+
+platforms/chibios/boards/GENERIC_PROMICRO_RP2040/configs/config.h:
